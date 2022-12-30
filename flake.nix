1552f8433122b12f88a2dff319ca210f2127e160
Jörg Thalheim <joerg@thalheim.io>
2022-12-30 11:08:00 +0100

    also expose aarch64

--- flake.nix
+++ flake.nix
@@ -11,16 +11,19 @@
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
 
-  outputs = { self, nixos-unstable, nixos-2211 }: {
-    packages.x86_64-linux = let
-      netboot = nixpkgs: (import (nixpkgs + "/nixos/release.nix") {}).netboot.x86_64-linux;
-      kexec-installer = nixpkgs: (nixpkgs.legacyPackages.x86_64-linux.nixos [self.nixosModules.kexec-installer]).config.system.build.kexecTarball;
+  outputs = { self, nixos-unstable, nixos-2211 }: let
+    supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
+    forAllSystems = nixos-unstable.lib.genAttrs supportedSystems;
+  in {
+    packages = forAllSystems (system: let
+      netboot = nixpkgs: (import (nixpkgs + "/nixos/release.nix") {}).netboot.${system};
+      kexec-installer = nixpkgs: (nixpkgs.legacyPackages.${system}.nixos [self.nixosModules.kexec-installer]).config.system.build.kexecTarball;
     in {
       netboot-nixos-unstable = netboot nixos-unstable;
       netboot-nixos-2211 = netboot nixos-2211;
       kexec-installer-nixos-unstable = kexec-installer nixos-unstable;
       kexec-installer-nixos-2211 = kexec-installer nixos-2211;
-    };
+    });
     nixosModules.kexec-installer = import ./nix/kexec-installer/module.nix;
     checks.x86_64-linux = {
       kexec-installer-unstable = nixos-unstable.legacyPackages.x86_64-linux.callPackage ./nix/kexec-installer/test.nix {};
