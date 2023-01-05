312e02e923e892f0ea8450afec7f61417ba59df8
Jörg Thalheim <joerg@thalheim.io>
2023-01-05 11:04:16 +0100

    kexec-installer: log network status after becoming online

--- nix/kexec-installer/module.nix
+++ nix/kexec-installer/module.nix
@@ -126,6 +126,25 @@ in {
     ];
   };
 
+  systemd.services.log-network-status = {
+    wantedBy = [ "multi-user.target" ];
+    # No point in restarting this. We just need this after boot
+    restartIfChanged = false;
+
+    serviceConfig = {
+      Type = "oneshot";
+      StandardOutput = "journal+console";
+      ExecStart = [
+        # Allow failures, so it still prints what interfaces we have even if we
+        # not get online
+        "-${pkgs.systemd}/lib/systemd/systemd-networkd-wait-online"
+        "${pkgs.iproute2}/bin/ip -c addr"
+        "${pkgs.iproute2}/bin/ip -c -6 route"
+        "${pkgs.iproute2}/bin/ip -c -4 route"
+      ];
+    };
+  };
+
   # Restore ssh host and user keys if they are available.
   # This avoids warnings of unknown ssh keys.
   boot.initrd.postMountCommands = ''
