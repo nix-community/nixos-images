d7dfa237d697e87aa5284b6a8ff4e073d422a3cb
Jörg Thalheim <joerg@thalheim.io>
2023-12-22 15:02:04 +0100

    switch out binary cache

--- flake.nix
+++ flake.nix
@@ -4,12 +4,8 @@
   inputs.nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   inputs.nixos-2311.url = "github:NixOS/nixpkgs/release-23.11";
 
-  nixConfig.extra-substituters = [
-    "https://cache.garnix.io"
-  ];
-  nixConfig.extra-trusted-public-keys = [
-    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-  ];
+  nixConfig.extra-substituters = [ "https://nix-community.cachix.org" ];
+  nixConfig.extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
 
   outputs = { self, nixos-unstable, nixos-2311 }:
     let
