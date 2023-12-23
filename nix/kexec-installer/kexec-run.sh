1dcf7a14ce847b6096801b1c1de6bf08a45b4b4d
Jörg Thalheim <joerg@thalheim.io>
2023-12-23 21:07:31 +0100

    fix kexec-syscall-auto check

--- nix/kexec-installer/kexec-run.sh
+++ nix/kexec-installer/kexec-run.sh
@@ -57,7 +57,7 @@ find . | cpio -o -H newc | gzip -9 >> "$SCRIPT_DIR/initrd"
 kexecSyscallFlags=""
 # only do kexec-syscall-auto on kernels newer than 6.0.
 # On older kernel we often get errors like: https://github.com/nix-community/nixos-anywhere/issues/264
-if ! printf "%s\n" "6.1" "$(uname -r)" | sort -c -V; then
+if printf "%s\n" "6.1" "$(uname -r)" | sort -c -V 2>&1; then
   kexecSyscallFlags="--kexec-syscall-auto"
 fi
 
