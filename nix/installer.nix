4de5aac8e9b8d8b839b0eaee61a893a41f0b968f
Jörg Thalheim <joerg@thalheim.io>
2024-09-22 17:56:58 +0200

    improve selecting latest zfs kernel

--- nix/installer.nix
+++ nix/installer.nix
@@ -4,15 +4,11 @@
   pkgs,
   ...
 }:
-let
-  latestZfsCompatibleLinuxPackages = lib.pipe pkgs.linuxKernel.packages [
-    builtins.attrValues
-    (builtins.filter (kPkgs: (builtins.tryEval kPkgs).success && kPkgs ? kernel && kPkgs.kernel.pname == "linux" && !kPkgs.zfs.meta.broken))
-    (builtins.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)))
-    lib.last
-  ];
-in
 {
+  imports = [
+    ./latest-zfs-kernel.nix
+    ./nix-settings.nix
+  ];
   # more descriptive hostname than just "nixos"
   networking.hostName = lib.mkDefault "nixos-installer";
 
@@ -24,7 +20,6 @@ in
 
   # use latest kernel we can support to get more hardware support
   boot.zfs.package = pkgs.zfsUnstable;
-  boot.kernelPackages = latestZfsCompatibleLinuxPackages;
 
   documentation.enable = false;
   documentation.man.man-db.enable = false;
@@ -40,10 +35,6 @@ in
     pkgs.rsync
     # alternative to nixos-generate-config
   ] ++ lib.optional (pkgs.lib.versionAtLeast lib.version "24.11") pkgs.nixos-facter;
-
-  imports = [
-    ./nix-settings.nix
-  ];
 
   # Don't add nixpkgs to the image to save space, for our intended use case we don't need it
   system.installer.channel.enable = false;
