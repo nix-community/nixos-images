3b99550fedca55481602f91ecc28a2d5e77d5dfc
Jörg Thalheim <joerg@thalheim.io>
2024-12-02 16:10:52 +0100

    compile samba with python3 minimal

--- nix/python-minimal.nix
+++ nix/python-minimal.nix
@@ -5,6 +5,10 @@
       cifs-utils = prev.cifs-utils.override { python3 = final.python3Minimal; };
       nfs-utils = prev.nfs-utils.override { python3 = final.python3Minimal; };
       talloc = prev.talloc.override { python3 = final.python3Minimal; };
+      samba = prev.samba.override { python3Packages = final.python3Minimal.pkgs; };
+      tevent = prev.tevent.override { python3 = final.python3Minimal; };
+      tdb = prev.tdb.override { python3 = final.python3Minimal; };
+      ldb = prev.ldb.override { python3 = final.python3Minimal; };
     })
   ];
 }
