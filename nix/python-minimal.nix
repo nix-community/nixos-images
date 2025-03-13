0d0b10965873ae7024dffd7469a240d72428fb4e
Jörg Thalheim <joerg@thalheim.io>
2025-03-13 12:45:46 +0100

    remove cifs from supported filesystem list
    samba is like 58MB large and in most cases shouldn't be necessary for
    installation (most often it would be only used at boot time rather than
    installation time). People that need samba are probably better off
    building their own installer. This helps to keep our memory usage again
    within 1GB

--- nix/python-minimal.nix
+++ nix/python-minimal.nix
@@ -2,13 +2,7 @@
   nixpkgs.overlays = [
     (final: prev: {
       bcachefs-tools = prev.bcachefs-tools.override { python3 = final.python3Minimal; };
-      cifs-utils = prev.cifs-utils.override { python3 = final.python3Minimal; };
       nfs-utils = prev.nfs-utils.override { python3 = final.python3Minimal; };
-      talloc = prev.talloc.override { python3 = final.python3Minimal; };
-      samba = prev.samba.override { python3Packages = final.python3Minimal.pkgs; };
-      tevent = prev.tevent.override { python3 = final.python3Minimal; };
-      tdb = prev.tdb.override { python3 = final.python3Minimal; };
-      ldb = prev.ldb.override { python3 = final.python3Minimal; };
     })
   ];
 }
