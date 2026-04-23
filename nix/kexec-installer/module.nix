7022c975470ea83fbc25a38e758d3376110d8695
a-kenji <aks.kenji@protonmail.com>
2026-04-23 16:45:47 +0200

    networkd: disable NetworkManager to avoid racing with systemd-networkd
    The kexec-installer and netboot-installer variants import both
    installation-device.nix (which enables NetworkManager) and
    networkd.nix (which enables systemd-networkd).
    
    After kexec, restore-network.service reapplies the static IP via
    systemd-networkd, but NetworkManager then activates on the same
    interface and drops the static route mid-install, breaking
    nixos-anywhere with "No route to host".
    
    Since these installer variants only use systemd-networkd, force
    NetworkManager off.

--- nix/kexec-installer/module.nix
+++ nix/kexec-installer/module.nix
@@ -34,6 +34,7 @@ in
 
   config = {
     boot.initrd.compressor = "xz";
+    networking.networkmanager.enable = lib.mkForce false;
     # This is a variant of the upstream kexecScript that also allows embedding
     # a ssh key.
     system.build.kexecRun = pkgs.runCommand "kexec-run" { } ''
